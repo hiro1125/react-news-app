12ea62b8929f51a790e3b1ff272ea2a48f01177b
hiro1125 <hiro112538@gmail.com>
2023-03-20 21:34:44 +0900

    feat: Textコンポーネントで文字の表示を追加
    文字のstyleを追加

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const sampleText = "text text text text text text text text text text text text text text text text"
+
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
       <View style={styles.leftContainer}>
         <Image style={{width: 100, height: 100}} source={{uri: "https://picsum.photos/id/10/300/300"}} />
       </View>
-      <View style={styles.rightContainer}></View>
-
+      <View style={styles.rightContainer} >
+        <Text numberOfLines={3} style={styles.text}>{sampleText}</Text>
+        <Text style={styles.subText}>ReactNews</Text>
+      </View>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -26,15 +31,22 @@ const styles = StyleSheet.create({
   itemContainer: {
     height: 100,
     width: "100%",
-    backgroundColor: "red",
+    backgroundColor: "white",
     flexDirection:"row",
   },
   leftContainer: {
     width: 100,
-    backgroundColor: "green",
   },
   rightContainer: {
     flex: 1,
-    backgroundColor: "blue",
+    padding: 10,
+    justifyContent: "space-between",
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: "gray",
   },
 });
