d428f044a9fc4a8f6bcff433ff9c1f12e035f30c
hiro1125 <hiro112538@gmail.com>
2023-03-23 20:33:07 +0900

    test: ダミーのデータの表示を追加

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import { ListItem } from './components/ListItem';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { ListItem } from "./components/ListItem";
+import articles from "./dummies/articles.json";
 
 export default function App() {
+  const items = articles.map((article, key) => {
+    return (
+      <ListItem
+        imageUrl={article.urlToImage}
+        title={article.title}
+        author={article.author}
+        key={key.toString()}
+      />
+    );
+  });
 
   return (
     <View style={styles.container}>
-      <ListItem
-      imageUrl={"https://picsum.photos/id/10/300/300"}
-      title="text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text"
-      author='React News' />
+      {items}
       <StatusBar style="auto" />
     </View>
   );
@@ -19,9 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#eee',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#eee",
+    alignItems: "center",
+    justifyContent: "center",
   },
-
 });
