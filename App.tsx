f1f57aa1148bbdbc03670420e834cbfc5bdff30b
hiro1125 <hiro112538@gmail.com>
2023-03-24 21:54:26 +0900

    feat: Scroll機能を追加

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import { ListItem } from "./components/ListItem";
 import articles from "./dummies/articles.json";
 
@@ -17,7 +17,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      {items}
+      <ScrollView>{items}</ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -27,7 +27,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#eee",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
