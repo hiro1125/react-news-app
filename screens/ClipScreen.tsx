0740740b59f3b94716c872f6f2c690e5b994c121
hiro1125 <hiro112538@gmail.com>
2023-03-29 09:10:36 +0900

    test: ClipScreenの仮実装

---
+++ screens/ClipScreen.tsx
@@ -0,0 +1,17 @@
+import {StyleSheet, SafeAreaView, Text} from "react-native";
+
+export const ClipScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>ClipScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "flex-start",
+  },
+});
