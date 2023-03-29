b2e01de54bc83365760099d8d2c9f4168bdd359a
hiro1125 <hiro112538@gmail.com>
2023-03-29 09:12:59 +0900

    feat: BottomTabNavigatorの実装
    機能として、タブをクリックすると複数の画面に切り替わるUI

--- App.tsx
+++ App.tsx
@@ -4,16 +4,28 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ArticleScreen } from "./screens/ArticleScreen";
 import { HomeScreen } from "./screens/HomeScreen";
 import { RootStackParamList } from "./types/type";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs"
+import { ClipScreen } from './screens/ClipScreen';
 
+const Tab = createBottomTabNavigator<RootStackParamList>();
 const Stack = createNativeStackNavigator<RootStackParamList>()
+
+const HomeStack = () => {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
+      <Stack.Screen name="Article" component={ArticleScreen} />
+    </Stack.Navigator>
+  )
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
-        <Stack.Screen name="Article" component={ArticleScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="HomeTab" component={HomeStack} options={{headerShown: false}} />
+        <Tab.Screen name="ClipTab" component={ClipScreen} options={{headerShown: false}} />
+      </Tab.Navigator>
     </NavigationContainer>
   )
 }
