837988f52a03f6e37ace781bb4b4655613f4f0fb
hiro1125 <hiro112538@gmail.com>
2023-03-30 18:47:41 +0900

    feat: タブにアイコンを追加
    型を追加

--- App.tsx
+++ App.tsx
@@ -1,31 +1,62 @@
-import 'expo-dev-menu';
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { ArticleScreen } from "./screens/ArticleScreen";
-import { HomeScreen } from "./screens/HomeScreen";
-import { RootStackParamList } from "./types/type";
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs"
-import { ClipScreen } from './screens/ClipScreen';
+import "expo-dev-menu";
+import {NavigationContainer, RouteProp} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {ArticleScreen} from "./screens/ArticleScreen";
+import {HomeScreen} from "./screens/HomeScreen";
+import {RootStackParamList} from "./types/type";
+import {
+  BottomTabScreenProps,
+  createBottomTabNavigator,
+} from "@react-navigation/bottom-tabs";
+import {ClipScreen} from "./screens/ClipScreen";
+import {FontAwesome} from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator<RootStackParamList>();
-const Stack = createNativeStackNavigator<RootStackParamList>()
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+type Props = {
+  color: string;
+  size: number;
+};
+
+const screenOption = ({route}: BottomTabScreenProps<RootStackParamList>) => ({
+  tabBarIcon: ({color, size}: Props) => {
+    if (route.name === "HomeTab") {
+      return <FontAwesome name="home" size={size} color={color} />;
+    } else if (route.name === "ClipTab") {
+      return <FontAwesome name="bookmark" size={size} color={color} />;
+    }
+  },
+});
 
 const HomeStack = () => {
-  return(
+  return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{headerShown: false}}
+      />
       <Stack.Screen name="Article" component={ArticleScreen} />
     </Stack.Navigator>
-  )
-}
+  );
+};
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="HomeTab" component={HomeStack} options={{headerShown: false}} />
-        <Tab.Screen name="ClipTab" component={ClipScreen} options={{headerShown: false}} />
+      <Tab.Navigator screenOptions={screenOption}>
+        <Tab.Screen
+          name="HomeTab"
+          component={HomeStack}
+          options={{headerShown: false, title: "Home"}}
+        />
+        <Tab.Screen
+          name="ClipTab"
+          component={ClipScreen}
+          options={{headerShown: false, title: "Clip"}}
+        />
       </Tab.Navigator>
     </NavigationContainer>
-  )
+  );
 }
