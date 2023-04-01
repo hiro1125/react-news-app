2413003402d760a2dab2c8b9a72915f27c8276cc
hiro1125 <hiro112538@gmail.com>
2023-04-02 01:11:13 +0900

    feat: Reduxのstore情報の表示を仮実装

--- screens/ArticleScreen.tsx
+++ screens/ArticleScreen.tsx
@@ -1,10 +1,10 @@
 import {RouteProp} from "@react-navigation/native";
 import {SafeAreaView, StyleSheet} from "react-native";
-import {RootStackParamList} from "../types/type";
+import {Article, RootStackParamList} from "../types/type";
 import {WebView} from "react-native-webview";
 import {ClipButton} from "../components/ClipButton";
-import {useDispatch} from "react-redux";
-import {addClip} from "../store/userSlice";
+import {useDispatch, useSelector} from "react-redux";
+import {addClip, deleteClip} from "../store/userSlice";
 
 type Props = {
   route: RouteProp<RootStackParamList, "Article">;
@@ -13,14 +13,20 @@ type Props = {
 export const ArticleScreen = ({route}: Props) => {
   const {article} = route.params;
   const dispatch = useDispatch();
+  const clips = useSelector((state: any) => state.user.clips);
+  const isClipped = clips.some((clip: any) => clip.url === article.url);
 
   const onPressClip = () => {
-    dispatch(addClip(article));
+    if (isClipped) {
+      dispatch(deleteClip(article));
+    } else {
+      dispatch(addClip(article));
+    }
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <ClipButton onPress={onPressClip} enabled={false} />
+      <ClipButton onPress={onPressClip} enabled={isClipped} />
       <WebView source={{uri: article.url}} />
     </SafeAreaView>
   );
