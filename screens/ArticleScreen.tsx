9dcdd73de9b2649d22f92ba21eb82392848b3361
hiro1125 <hiro112538@gmail.com>
2023-04-02 00:16:02 +0900

    feat: ClipButtonの作成
    actionのdispatchを実装

--- screens/ArticleScreen.tsx
+++ screens/ArticleScreen.tsx
@@ -1,25 +1,34 @@
-import { RouteProp } from "@react-navigation/native";
-import { SafeAreaView, StyleSheet } from "react-native"
-import { RootStackParamList } from "../types/type";
+import {RouteProp} from "@react-navigation/native";
+import {SafeAreaView, StyleSheet} from "react-native";
+import {RootStackParamList} from "../types/type";
 import {WebView} from "react-native-webview";
-
+import {ClipButton} from "../components/ClipButton";
+import {useDispatch} from "react-redux";
+import {addClip} from "../store/userSlice";
 
 type Props = {
-  route: RouteProp<RootStackParamList,"Article">
-}
+  route: RouteProp<RootStackParamList, "Article">;
+};
 
 export const ArticleScreen = ({route}: Props) => {
   const {article} = route.params;
-  return(
+  const dispatch = useDispatch();
+
+  const onPressClip = () => {
+    dispatch(addClip(article));
+  };
+
+  return (
     <SafeAreaView style={styles.container}>
+      <ClipButton onPress={onPressClip} enabled={false} />
       <WebView source={{uri: article.url}} />
     </SafeAreaView>
-  )
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "FFF"
+    backgroundColor: "FFF",
   },
 });
