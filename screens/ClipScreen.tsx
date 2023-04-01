3a7169c009cb577570a873c7d86ef705dd039c81
hiro1125 <hiro112538@gmail.com>
2023-04-02 01:31:55 +0900

    feat: ClipScreen画面の仮実装

--- screens/ClipScreen.tsx
+++ screens/ClipScreen.tsx
@@ -1,12 +1,32 @@
-import {StyleSheet, SafeAreaView, Text} from "react-native";
+import {NativeStackNavigationProp} from "@react-navigation/native-stack";
+import {StyleSheet, SafeAreaView, FlatList} from "react-native";
+import {useSelector} from "react-redux";
+import {ListItem} from "../components/ListItem";
+import {RootStackParamList} from "../types/type";
 
-export const ClipScreen = () => {
+type Props = {
+  navigation: NativeStackNavigationProp<RootStackParamList, "Clip">;
+};
+
+export const ClipScreen = ({navigation}: Props) => {
+  const clips = useSelector((state: any) => state.user.clips);
   return (
     <SafeAreaView style={styles.container}>
-      <Text>ClipScreen</Text>
+      <FlatList
+        data={clips}
+        renderItem={({item}) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+            onPress={() => navigation.navigate("Article", {article: item})}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
