3fd2913c6f479f8771e71860061c331aea3f849c
hiro1125 <hiro112538@gmail.com>
2023-04-02 01:35:17 +0900

    fix: ClipTabを開くとHomeTabのニュース記事が表示されるエラーの修正

--- App.tsx
+++ App.tsx
@@ -44,6 +44,19 @@ const HomeStack = () => {
   );
 };
 
+const ClipStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Clip"
+        component={ClipScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen name="Article" component={ArticleScreen} />
+    </Stack.Navigator>
+  );
+};
+
 export default function App() {
   return (
     <Provider store={store}>
@@ -56,7 +69,7 @@ export default function App() {
           />
           <Tab.Screen
             name="ClipTab"
-            component={ClipScreen}
+            component={ClipStack}
             options={{headerShown: false, title: "Clip"}}
           />
         </Tab.Navigator>
