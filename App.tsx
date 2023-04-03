e9026376afae8a6c595b84e5690ffaaa5070e7ae
hiro1125 <hiro112538@gmail.com>
2023-04-03 19:14:44 +0900

    feat: redux-persistの実装
    機能として、reduxの状態を永続化

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import {Provider} from "react-redux";
-import {store} from "./store";
+import {PersistGate} from "redux-persist/integration/react";
+import {store, persistor} from "./store";
 import {RootNavigator} from "./navigation/RootNavigator";
 export default function App() {
   return (
     <Provider store={store}>
-      <RootNavigator />
+      <PersistGate loading={null} persistor={persistor}>
+        <RootNavigator />
+      </PersistGate>
     </Provider>
   );
 }
--- store/index.ts
+++ store/index.ts
@@ -1,12 +1,27 @@
 import {configureStore} from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import {combineReducers} from "redux";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {persistStore, persistReducer} from "redux-persist";
 import {TypedUseSelectorHook, useSelector} from "react-redux";
+
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+};
+
+const rootReducer = combineReducers({
+  users: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    users: userReducer,
-  },
+  reducer: persistedReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 
 export const useRootSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const persistor = persistStore(store);
